18fac139c89159a784a60c19e25e8ee96fe06ad4
nacika <nacika.inscatolare@gmail.com>
2014-12-05 02:26:12 +0900

    first commit

---
+++ swiftmetal/swiftmetal/MyPlayground.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Swiftのgitignore
+var hoge = "http://stackoverflow.com/questions/18939421/what-should-xcode-6-gitignore-file-include";
